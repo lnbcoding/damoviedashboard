cbb1932d200b35857b3b1182a1063a146dacaa5a
lnbcoding <leonardbui@gmail.com>
2015-01-25 15:29:59 -0800

    add /movies get route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,13 @@
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  redirect '/movies'
+end
+
+get '/movies' do
+  headers = { :accept => 'application/json' }
+
+  find_top_movies = HTTParty.get("http://api.themoviedb.org/3/movie/popular?api_key=#{ENV['MOVIE_KEY']}", headers)
+
+  @top_movies = find_top_movies['results'].take(15)
+
+  erb :search
 end
